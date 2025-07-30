d5816b0fa05e64a09bd8aa4a61fb407f15429921
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-30 14:11:51 +0200

    changes

--- hc_sr04.h
+++ hc_sr04.h
@@ -3,6 +3,10 @@
 
 #include "hc_sr04_config.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
     hc_sr04_config_t config;
     hc_sr04_interface_t interface;
@@ -19,5 +23,9 @@ hc_sr04_err_t hc_sr04_deinitialize(hc_sr04_t* hc_sr04);
 hc_sr04_err_t hc_sr04_transmit_trig_signal(hc_sr04_t const* hc_sr04);
 hc_sr04_err_t hc_sr04_receive_echo_signal(hc_sr04_t* hc_sr04);
 uint32_t hc_sr04_get_distance_mm(hc_sr04_t* hc_sr04);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // HC_SR04_HC_SR04_H
--- hc_sr04_config.h
+++ hc_sr04_config.h
@@ -4,6 +4,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {
     HC_SR04_ERR_OK = 0,
     HC_SR04_ERR_FAIL = 1 << 0,
@@ -31,5 +35,9 @@ typedef struct {
     hc_sr04_err_t (*timer_set_count)(void*, uint32_t);
     hc_sr04_err_t (*timer_delay_us)(void*, uint32_t);
 } hc_sr04_interface_t;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // HC_SR04_HC_SR04_CONFIG_H
